9e8819315ed47801565a6d079faacdf2f022c68e
Tim <timothyseeyl@gmail.com>
2024-10-30 17:04:12 +0800

    final_for_real

--- task02/src/vttp/batch5/sdf/task02/Main.java
+++ task02/src/vttp/batch5/sdf/task02/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		
-		String fileTest = "task02\\TTT\\board4.txt";
+		String fileTest = args[0];
 		
 		//filereaders setup
 		File file = new File(fileTest);
